3a284caf70bc430caca589b7352ad5f6b18e72a1
Alex Miranda <alexmirandamoraes@gmail.com>
2023-06-03 18:33:53 +0200

    Day 15 - part 2

--- src/main/java/net/alexmiranda/adventofcode2021/Day15.java
+++ src/main/java/net/alexmiranda/adventofcode2021/Day15.java
@@ -3,35 +3,62 @@ package net.alexmiranda.adventofcode2021;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.io.Writer;
 import java.util.*;
 
 public class Day15 {
     record Location(int x, int y) {}
     record Search(Location loc, int risk) {}
 
-    public static int[][] readInput(Reader reader) throws IOException {
+    public static byte[][] readInput(Reader reader) throws IOException {
         try (var br = new BufferedReader(reader)) {
-            var input = new ArrayList<int[]>();
+            var input = new ArrayList<byte[]>();
             String line;
             while ((line = br.readLine()) != null) {
-                var row = new int[line.length()];
+                var row = new byte[line.length()];
                 for (int i = 0; i < line.length(); i++) {
-                    row[i] = Character.digit(line.charAt(i), 10);
+                    row[i] = (byte) Character.digit(line.charAt(i), 10);
                 }
                 input.add(row);
             }
-            return input.toArray(int[][]::new);
+            return input.toArray(byte[][]::new);
         }
     }
 
-    public static int lowestTotalRisk(int[][] input) {
+    public static byte[][] readFullMap(Reader reader) throws IOException {
+        var input = readInput(reader);
+        int w = input[0].length;
+        int h = input.length;
+        var map = new byte[h * 5][w * 5];
+
+        for (int i = 0; i < h; i++) {
+            System.arraycopy(input[i], 0, map[i], 0, w);
+        }
+
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                for (byte k = 1; k <= 4; k++) {
+                    byte v = (byte) (1 + ((map[i][j] + k) - 1) % 9);
+                    map[i][j + k * w] = v;
+                    map[i + k * h][j] = v;
+                    for (byte l = 1; l <= 4; l++) {
+                        byte vv = (byte) (1 + ((v + l) - 1) % 9);
+                        map[i + k * h][l * w + j] = vv;
+                    }
+                }
+            }
+        }
+
+        return map;
+    }
+
+    public static int lowestTotalRisk(byte[][] input) {
         int h = input.length - 1;
         int w = input[0].length - 1;
         var start = new Location(0, 0);
         var goal = new Location(w, h);
 
-        var seen = new HashSet<Location>();
-        var queue = new PriorityQueue<>(Comparator.comparingInt(Search::risk));
+        var queue = new PriorityQueue<>(h + w, Comparator.comparingInt(Search::risk));
         queue.add(new Search(start, 0));
         while (!queue.isEmpty()) {
             var s = queue.poll();
@@ -41,9 +68,17 @@ public class Day15 {
                 return s.risk;
             }
 
-            seen.add(s.loc);
-            for (var neighbour : neighbours(s.loc, w, h, seen)) {
-                int risk = s.risk + input[neighbour.y][neighbour.x];
+            // have we visited this node before?
+            if (input[s.loc.y][s.loc.x] > 10) {
+                continue;
+            }
+
+            // Because we know the range of each cell is from 1 to 9, then we can mark the node as visited by adding 10
+            input[s.loc.y][s.loc.x] += 10;
+
+            // plan neighbours for visiting later
+            for (var neighbour : neighbours(s.loc, w, h, input)) {
+                int risk = s.risk + input[neighbour.y][neighbour.x] % 10;
                 queue.add(new Search(neighbour, risk));
             }
         }
@@ -51,13 +86,13 @@ public class Day15 {
         throw new IllegalStateException("Destination is not reachable");
     }
 
-    private static List<Location> neighbours(Location loc, int w, int h, Set<Location> seen) {
+    private static List<Location> neighbours(Location loc, int w, int h, byte[][] input) {
         var neighbours = new ArrayList<Location>(4);
 
         // right
         if (loc.x < w) {
             var target = new Location(loc.x + 1, loc.y);
-            if (!seen.contains(target)) {
+            if (input[target.y][target.x] < 10) { // check if target has been visited
                 neighbours.add(target);
             }
         }
@@ -65,7 +100,7 @@ public class Day15 {
         // bottom
         if (loc.y < h) {
             var target = new Location(loc.x, loc.y + 1);
-            if (!seen.contains(target)) {
+            if (input[target.y][target.x] < 10) { // check if target has been visited
                 neighbours.add(target);
             }
         }
@@ -73,7 +108,7 @@ public class Day15 {
         // left
         if (loc.x > 0) {
             var target = new Location(loc.x - 1, loc.y);
-            if (!seen.contains(target)) {
+            if (input[target.y][target.x] < 10) { // check if target has been visited
                 neighbours.add(target);
             }
         }
@@ -81,11 +116,20 @@ public class Day15 {
         // top
         if (loc.y > 0) {
             var target = new Location(loc.x, loc.y - 1);
-            if (!seen.contains(target)) {
+            if (input[target.y][target.x] < 10) { // check if target has been visited
                 neighbours.add(target);
             }
         }
 
         return neighbours;
+    }
+
+    static void print(Writer w, byte[][] map) throws IOException {
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i].length; j++) {
+                w.write(Character.forDigit(map[i][j], 10));
+            }
+            w.write('\n');
+        }
     }
 }
